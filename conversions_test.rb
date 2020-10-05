ac72c49ef5f16811dc61ad148f5e143077f4da61
Sara Kantor <sara.kantor.nv@gmail.com>
2020-10-05 22:36:48 +0000

    Done.

--- conversions_test.rb
+++ conversions_test.rb
@@ -15,3 +15,9 @@ if answer == 28.3495
 else
   puts "Test failed: got #{answer} instead"
 end
+
+ruby describe "conversion" do describe '#ounces_to_grams' 
+do it 'given zero, returns 0.0' do grams = ounces_to_grams(0) expect(grams).to eq(0.0)
+end
+end
+end
